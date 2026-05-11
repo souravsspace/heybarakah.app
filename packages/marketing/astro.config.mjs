fde27619c0f2709a0bac8a8019b6c9501b34c8b7
Sana <88476413+souravsspace@users.noreply.github.com>
2026-05-12 01:51:35 +0600

    refactor(marketing): update astro config

--- packages/marketing/astro.config.mjs
+++ packages/marketing/astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
