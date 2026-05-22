10985ffd9c40d1b97a4ef2aec10b665c71bb5046
Sana <88476413+souravsspace@users.noreply.github.com>
2026-05-22 17:06:35 +0600

    feat(lib): add revenuecat validators

--- packages/core/src/subscriptions/validators.ts
+++ packages/core/src/subscriptions/validators.ts
@@ -6,3 +6,18 @@ export const productId = v.union(
   v.literal("family"),
   v.literal("lifetime")
 );
+
+export const revenueCatStore = v.union(
+  v.literal("app_store"),
+  v.literal("play_store"),
+  v.literal("stripe"),
+  v.literal("promotional"),
+  v.literal("mac_app_store"),
+  v.literal("amazon")
+);
+
+export const revenueCatPeriodType = v.union(
+  v.literal("normal"),
+  v.literal("trial"),
+  v.literal("intro")
+);
